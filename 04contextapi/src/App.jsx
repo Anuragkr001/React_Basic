49b406afcdb6673d6801436540253bfe7fb81e76
Anuragkr001 <mranuragkumar91@gmail.com>
2026-01-02 19:39:55 +0530

    context API

---
+++ 04contextapi/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Login from './components/login'
+import Profile from './components/profile'
+import UserContextProvider from './context/usercontextprovider'
+
+
+function App() {
+
+  return (
+    
+    <UserContextProvider>
+     <h1>React Project</h1>
+     <Login />
+     <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
